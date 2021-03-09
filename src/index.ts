23fc33f14f57e290d455b019bfc25634fab15b5f
saqla <whitecross8fortune8fatedress@gmail.com>
2021-03-09 21:20:36 +0900

    関数にアノテーション

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,25 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { unknownSample, anySample, notExistSample, primitiveSample } from './basic'
+// import { unknownSample, anySample, notExistSample, primitiveSample } from './basic'
 
-unknownSample()
-anySample()
-notExistSample()
-primitiveSample()
+// unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+
+// 04.関数の型定義
+import { logMessage, logMessage2, logMessage3, logMessage4, logMessage6, logMessage7 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript!')
+logMessage2('Hello TypeScript!')
+logMessage3('Hello TypeScript!')
+logMessage4('Hello TypeScript!')
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5:', sum)
+logMessage6('Hello signature')
+logMessage7('Hello signature')
